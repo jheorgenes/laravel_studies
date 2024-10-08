bf0ba9903250e91d3f825345d970b8a594266c11
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-10-08 16:51:18 -0300

    -- Entendendo melhor sobre Rotas no Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.main_layout')
 @section('content')
 
-<p class="display-6 text-secondary text-center py-5">CONTENT</p>
+{{-- Apresentar myName a partir da route::view --}}
+@if (!empty($myName)) {
+    <p>{{ $myName }}</p>
+}
+@endif
 
 @endsection
