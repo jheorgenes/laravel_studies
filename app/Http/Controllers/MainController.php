2ece1c4c0e1425b8dbc1cae4ff8d33ad75d14091
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-10-08 17:38:18 -0300

    -- Aprendendo a utilizar o recurso resources no laravel para criar métodos e definir rotas

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class MainController extends Controller
+{
+    public function initMethod(): string
+    {
+        return "Hello World";
+    }
+
+    public function viewPage(): View
+    {
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,24 @@
 <?php
 
+use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProdutsController;
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/init', [MainController::class, 'initMethod'])->name('init');
+Route::get('/view', [MainController::class, 'viewPage'])->name('view');
+
+// route para controller single action
+Route::get('/single', SingleActionController::class)->name('single');
+
+// route para controller do tipo resource
+// Route::resource('users', UserController::class);
+
+Route::resources([
+    'users' => UserController::class,
+    'clients' => ClientsController::class,
+    'products' => ProdutsController::class
+]);
